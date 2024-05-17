cb856957d7102436b977fe26bf98b083f931ec5f
Navneet Choudhary <nkaurchoudhary@gmail.com>
2024-05-16 22:29:26 -0400

    Homwork 2 Completed

---
+++ 03_homework/homework2.sql
@@ -0,0 +1,33 @@
+/* SELECT *
+FROM customer */
+
+/*SELECT *
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10*/
+
+/*SELECT * FROM customer_purchases
+WHERE product_id = 4 AND 9 */
+
+/*SELECT * , quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE vendor_id BETWEEN 8 AND 10*/
+
+/*SELECT product_id, product_name
+,CASE WHEN product_qty_type != 'unit' THEN 'bulk' 
+Else product_qty_type END AS prod_qty_type_condensed
+FROM product */
+
+/*SELECT product_id, product_name
+,CASE WHEN product_qty_type != 'unit' THEN 'bulk' 
+Else product_qty_type END AS prod_qty_type_condensed
+,CASE WHEN product_name LIKE '%pepper%' THEN '1'
+ELSE '0' END AS pepper_flag
+FROM product */
+
+/* SELECT *
+FROM vendor v
+INNER JOIN vendor_booth_assignments vba
+ON v.vendor_id = vba.vendor_id
+ORDER BY vendor_name, market_date */
+
