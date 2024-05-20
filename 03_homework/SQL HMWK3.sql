9ceb0a50644c963e2619d904212459b190a21659
Navneettc <167141238+Navneettc@users.noreply.github.com>
2024-05-20 17:10:02 -0400

    Update SQL HMWK3.sql

--- 03_homework/SQL HMWK3.sql
+++ 03_homework/SQL HMWK3.sql
@@ -4,12 +4,14 @@
 FROM vendor_booth_assignments
 GROUP BY vendor_id */
 
+
 /*SELECT customer_last_name, customer_first_name, quantity, cost_to_customer_per_qty, sum(quantity * cost_to_customer_per_qty) as total_spent
 FROM customer
 Inner JOIN customer_purchases ON customer.customer_id = customer_purchases.customer_id
 GROUP By customer_last_name, customer_first_name
 Having total_spent > 2000
 ORDER BY customer_last_name, customer_first_name */
+
 
 
 /*DROP TABLE IF EXISTS new_vendor;
@@ -25,14 +27,19 @@ SELECT *
 FROM new_vendor */
 
 
+
+
 /*SELECT customer_id, strftime('%m',market_date) as month, strftime('%Y', market_date) as year
 FROM customer_purchases*/
+
 
 
 /* SELECT customer_id, strftime('%m',market_date) as month, strftime('%Y', market_date) as year, sum(quantity * cost_to_customer_per_qty) as total_spent
 FROM customer_purchases
 WHERE strftime('%Y-%m',market_date) = '2019-04'
 GROUP BY customer_id */
+
+
 
 
 
